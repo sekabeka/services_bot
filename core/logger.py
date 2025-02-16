327a8b9dd5fbf050f6e61e0550ad68e6d889d114
sekabeka <a.brezgin@g.nsu.ru>
2025-02-16 16:07:26 +0700

    v1

---
+++ core/logger.py
@@ -0,0 +1,22 @@
+import logging
+import inspect
+
+from loguru import logger
+
+logger.add("logs/log.log", level="DEBUG", rotation="10 MB")
+
+class InterceptHandler(logging.Handler):
+    def emit(self, record: logging.LogRecord) -> None:
+        level: str | int
+        try:
+            level = logger.level(record.levelname).name
+        except ValueError:
+            level = record.levelno
+
+        frame, depth = inspect.currentframe(), 0
+        while frame and (depth == 0 or frame.f_code.co_filename == logging.__file__):
+            frame = frame.f_back
+            depth += 1
+
+        logger.opt(depth=depth, exception=record.exc_info).log(level, record.getMessage())
+
